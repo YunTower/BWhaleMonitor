ff52c88f26692ab8b9731ec499bd9593e6cad518
fish <xyfishcn@qq.com>
2024-11-22 14:46:25 +0800

    pref: 优化请求功能

--- src/utils/requester.ts
+++ src/utils/requester.ts
@@ -1,9 +1,14 @@
-import axios, { type AxiosError } from 'axios'
+import axios, { type AxiosError, type AxiosResponse } from 'axios'
 import type { AxiosInstance, InternalAxiosRequestConfig } from 'axios'
 import { createDiscreteApi } from 'naive-ui'
-const { message} = createDiscreteApi(
-  ['message'],
-)
+
+export interface BaseResponseType<T> {
+  code: number
+  data: T
+  msg: string
+}
+
+const { message } = createDiscreteApi(['message'])
 
 const requester: AxiosInstance = axios.create({
   // 设置基础路径
@@ -22,26 +27,25 @@ requester.interceptors.request.use(
 )
 
 requester.interceptors.response.use(
-  async (response: any) => {
-    // 判断请求是否正常
-    if (response.status !== 200) {
-      if (response.data.code === 429) {
-        message.error(response.data?.msg ?? '请求过于频繁，请稍后再试！')
-      }
-      if (response.status !== 404) {
-        return Promise.reject(response)
-      }
-    } else {
-      return response.data
+  async (response: AxiosResponse) => {
+    const baseResponse: BaseResponseType<any> = {
+      code: response.data.code,
+      data: response.data.data,
+      msg: response.data.msg,
     }
+    return baseResponse
   },
   (error: AxiosError) => {
-    if (error.status === 404) {
+    if (error.response?.status === 404) {
       message.error('404 页面/接口不存在')
     }
-    if (error.status === 500) {
+    if (error.response?.status === 500) {
       message.error('服务器错误，请联系管理员修复')
     }
+    if (error.response?.status === 429) {
+      message.error('请求过于频繁，请稍后再试！')
+    }
+
     if (
       error.code === 'ECONNABORTED' ||
       error.message === 'Network Error' ||
@@ -53,4 +57,10 @@ requester.interceptors.response.use(
   },
 )
 
-export default requester
+interface ExtendedAxiosInstance extends AxiosInstance {
+  get<T>(url: string, config?: any): Promise<BaseResponseType<T>>
+}
+
+const extendedRequester: ExtendedAxiosInstance = requester as ExtendedAxiosInstance
+
+export default extendedRequester
