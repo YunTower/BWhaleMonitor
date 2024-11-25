63fe70f288a465561c47382885b66b1c6acde797
fish <xyfishcn@qq.com>
2024-11-25 12:26:37 +0800

    add(types): 添加d.ts文件

---
+++ types/index.d.ts
@@ -0,0 +1,41 @@
+export interface BaseResponseType<T> {
+  code: number
+  data: T
+  msg: string
+}
+
+export interface userType {
+  username: string
+  role: 'admin' | 'visitor'
+  isLogin: boolean
+}
+
+export interface ServerItemType {
+  id: number
+  name: string
+  cpu: number
+  memory: number
+  disk: number
+  cpu_use: number
+  memory_use: number
+  disk_use: number
+  os: string
+  ip: string
+  location: string
+  net_download: number
+  net_upload: number
+  status: 1 | 0
+}
+
+export interface ServerInfoType {
+  id: number
+  name: string
+  os: string
+  ip: string
+  location: string
+  cpu: number
+  memory: number
+  disk: number
+  network: string
+  status: number
+}
