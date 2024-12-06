dcb43ead2c71a6135cd74571fdb1fb157d64e7ca
fish <xyfishcn@qq.com>
2024-12-06 15:05:58 +0800

    add(copyright): 增加版权信息

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,6 @@ app.use(naive)
 app.use(router)
 
 app.mount('#app')
+
+
+console.log("%c BWhaleMonitor 蓝鲸服务器监控探针 by YunTower","font-size:18px;color:#fff;background-image: linear-gradient(252deg, #63e2b7 0%, rgb(42, 148, 125) 100%);")
